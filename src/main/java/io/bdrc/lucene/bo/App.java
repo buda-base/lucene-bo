03f9e63893526e1e492e77b91371246a8f487bd2
drupchen <hhdrupchen@gmail.com>
2017-04-10 20:04:48 +0200

    initial commit

---
+++ src/main/java/io/bdrc/lucene/bo/App.java
@@ -0,0 +1,13 @@
+package io.bdrc.lucene.bo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
