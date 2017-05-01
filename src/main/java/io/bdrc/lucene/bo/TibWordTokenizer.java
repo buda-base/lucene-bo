abdf7f557e1a40df3b9f6db66f7b133cb58f8391
drupchen <hhdrupchen@gmail.com>
2017-05-01 11:27:16 +0200

    how to have the input ?

--- src/main/java/io/bdrc/lucene/bo/TibWordTokenizer.java
+++ src/main/java/io/bdrc/lucene/bo/TibWordTokenizer.java
@@ -27,7 +27,10 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
 import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.apache.lucene.util.PagedBytes.Reader;
 
+import io.bdrc.lucene.stemmer.Optimizer;
+import io.bdrc.lucene.stemmer.Row;
 import io.bdrc.lucene.stemmer.Trie;
 
 public final class TibWordTokenizer extends Tokenizer {
@@ -48,22 +51,58 @@ public final class TibWordTokenizer extends Tokenizer {
 	private void init() throws FileNotFoundException, IOException {
 		this.scanner = new Trie(true);
 		
+//		currently only adds the entries without any diff
 		String file = "./resource/output/total_lexicon.txt";
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
 		        String[] parts = line.split(" ");
-		        
-		    	System.out.println(line);
+		        this.scanner.add(parts[0], "X");
 		    }
+			Optimizer opt = new Optimizer();
+			this.scanner.reduce(opt);
 		}
-
 	}
 
 
 	@Override
 	public boolean incrementToken() throws IOException {
-		// TODO Auto-generated method stub
+		Row row = scanner.getRow(scanner.getRoot());
+		while (offsetAtt < this.setReader.length()) {
+			Character ch = toAnalyze.charAt(i); // get the current character
+			System.out.println("moving to index "+i+": "+ch);
+			w = now.getCmd(ch); // get the command associated with the current character at next step in the Trie
+			if (w >= 0) {
+				if (i >= toAnalyze.length()-1 || !isTibLetter(toAnalyze.charAt(i+1))) {
+						System.out.println("current row has an command for it, so it's a match");
+						lastCmdIndex = w;
+						lastCharIndex = i;
+					}
+            } else {
+//            	System.out.println("current row does not have a command for it, no match");
+            }
+			w = now.getRef(ch); // get the next row if there is one
+			if (w >= 0) {
+//				System.out.println("current row does have a reference for this char, further matches are possible, moving one row forward in the Trie");
+                now = t.getRow(w);
+            } else {
+//            	System.out.println("current row does not have a reference to this char, so there's no further possible match, breaking the loop");
+                break; // no more steps possible in our research
+            }
+			i++;
+		}
+		//w = now.getCmd(toAnalyze.charAt(i));
+		if (lastCharIndex == -1) {
+			System.out.println("I have found nothing");
+			return;
+		}
+		System.out.println("I have found a token that goes from "+startCharIndex+" to "
+				+ lastCharIndex);
+		System.out.println("the substring is: "+toAnalyze.substring(startCharIndex, lastCharIndex+1));
+		System.out.println("the command associated with this token in the Trie is: "+t.getCommandVal(lastCmdIndex));
+		
+		OffsetAttribute i = offsetAtt;
+		
 		return false;
 	}
 	
